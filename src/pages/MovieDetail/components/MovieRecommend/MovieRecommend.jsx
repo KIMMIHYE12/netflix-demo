8eb90c2a49bad2c027b9b754521d9a2b3897f4ce
sapiens12 <sapiens12@nate.com>
2024-04-13 12:45:48 +0200

    오류 수정 test

--- src/pages/MovieDetail/components/MovieRecommend/MovieRecommend.jsx
+++ src/pages/MovieDetail/components/MovieRecommend/MovieRecommend.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { useMovieRecommendQuery } from "../../../../hooks/useMovieRecommend";
+import { responsive } from "../../../../constans/reponsive";
+import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import MovieCard from "../../../../common/MovieCard/MovieCard";
 
 const MovieRecommend = ({ id }) => {
   const { data: recommend } = useMovieRecommendQuery({ id });
   console.log("추천", recommend);
 
-  return <div>recomend</div>;
+  return (
+    <div>
+      추천영화
+      {recommend.map((movie, index) => (
+        <MovieCard movie={movie} key={index} />
+      ))}
+      {/* <Carousel
+        infinite={true}
+        centerMode={true}
+        itemClass='movie-slider p-1'
+        containerClass='carousel-container'
+        responsive={responsive}
+      >
+        {recommend.map((movie, index) => (
+          <MovieCard movie={movie} key={index} />
+        ))}
+      </Carousel> */}
+    </div>
+  );
 };
 
 export default MovieRecommend;
